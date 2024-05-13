1192c8c8c9cdcee04f514e010b368dcafcbb621d
Muhammed <aydin.muhammedemin67@gmail.com>
2024-05-14 00:34:18 +0200

    mise a jour npm, lien avec JS et modif html en php

--- src/index.php
+++ src/index.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="./output.css" rel="stylesheet">
+  <link href="./output.css" rel="stylesheet"> <!-- lien avec tailwind -->
+  <script src="./script.js"></script> <!-- lien avec mon script -->
   <title>Mon Portfolio</title>
 </head>
 <body>
@@ -15,6 +16,6 @@
   </h1>
 
 
-
+  
 </body>
 </html>
